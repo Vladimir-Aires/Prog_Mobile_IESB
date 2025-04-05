7b4d287e2e7d357a7dc0a8ab332b8c25aca6d5f9
Vladimir-Aires <vlad.aires05@gmail.com>
2025-04-04 21:41:26 -0300

    exercicios e assuntos de sala

---
+++ Prog_Mobile_IESB/exercicios/lista-times-jogadores/components/Time.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, FlatList } from "react-native";
+import { Card, Text } from "react-native-paper";
+import React from "react";
+import Jogador from "./Jogador";
+
+export default function Time(props) {
+    const { nome, anoFundacao, mascote, imagem, jogadores } = props;
+
+    return (
+        <Card style={{ margin: 10 }}>
+            <Card.Title title={nome} subtitle={anoFundacao} />
+            <Card.Content>
+                <Text>Mascote: {mascote}</Text>
+            </Card.Content>
+            <Card.Cover source={{ uri: imagem }} />
+            <Card.Actions>
+                <FlatList
+                    horizontal
+                    data={jogadores}
+                    renderItem={({ item }) => (
+                        <Jogador
+                            nome={item.nome}
+                            numero={item.numero}
+                            imagem={item.imagem}
+                        />
+                    )}
+                />
+            </Card.Actions>
+        </Card>
+    );
+}
+
+const styles = StyleSheet.create({});
